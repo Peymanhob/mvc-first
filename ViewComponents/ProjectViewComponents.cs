c3e25c887f2946cd4a0fdce0a86be24dace1debf
MSC <MOSTAFA.SABERY@GMAIL.COM>
2023-12-26 13:31:27 +0330

    clean

--- ViewComponents/ProjectViewComponents.cs
+++ ViewComponents/ProjectViewComponents.cs
@@ -11,7 +11,7 @@ namespace mvc_first.ViewComponents
             {
                 new Project(1, "مقاله اول", "گوشی اول","~/images/project/project-1.jpg"),
                 new Project(2, "مقاله دوم", "گوشی دوم","~/images/project/project-2.jpg"),
-                new Project(3, "مقاله سوم", "گوشی سوم","~/images/project/project-2.jpg"),
+                new Project(3, "مقاله سوم", "گوشی سوم","~/images/project/project-3.jpg"),
                 new Project(4, "مقاله چهارم", "گوشی چهارم","~/images/project/project-4.jpg")
             };
             return View("_Project", _projrct);
@@ -19,4 +19,5 @@ namespace mvc_first.ViewComponents
 
 
         }
+    }
 }
